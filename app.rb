34db436176ac9036560971b26582bc3158101a09
Junaid Farooq <junaid@evercam.io>
2016-03-02 22:11:54 +0500

    Added routes for testing

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require "sinatra/base"
 
 class App < Sinatra::Base
+
 	get "/" do
 		"Hello World"
+	end
+
+	post "/" do
+		"am post"
+	end
+
+	put "/" do
+		"am put can be patch"
+	end
+
+	delete "/" do
+		"can be deleted"
+	end
+
+	get "/hello/:name" do |f|
+		"hello  #{f}"
 	end
 end
